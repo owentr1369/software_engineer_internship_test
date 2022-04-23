8dc65ff84def81984f5c6ef480a3b0ac5e997da4
owentr1369 <tamvaa1306@gmail.com>
2022-04-23 11:12:37 +0700

    add axios and post api

--- src/components/1a/1a.js
+++ src/components/1a/1a.js
@@ -1,14 +1,14 @@
-// const findMax5=[3,4,5,3,2,3,10,11]
+// Cho mảng số nguyên chưa được sắp xếp. Viết hàm xếp 5 số lớn nhất ra đầu mảng.
+
+// const findMax5 = [3, 4, 5, 3, 2, 3, 10, 11];
 // Output: [11,10,5,4,3]
 // const findMax5=[14,12,38,17,10,36,12,29,45,34,48,22]
 // Output: > [48, 45, 38, 36, 34]
-const findMax5 = [10, 11, 2, 30, 22, 6, 8, 9, 11, 12, 22];
+// const findMax5 = [10, 11, 2, 30, 22, 6, 8, 9, 11, 12, 22];
 // Output: > [30, 22, 22, 12, 11]
 
-var max5 = [];
-
 function Max5() {
-  max5 = findMax5.sort((a, b) => b - a).slice(0, 5);
+  let max5 = findMax5.sort((a, b) => b - a).slice(0, 5);
   console.log(max5);
   return <div></div>;
 }
--- src/components/2/form.js
+++ src/components/2/form.js
@@ -1,47 +1,63 @@
 import React from "react";
 import { useState } from "react";
+import Axios from "axios";
 
 const Form = () => {
   const [val, setVal] = useState();
-  function saveData() {
-    //
-  }
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [notes, setNotes] = useState("");
+  const saveData = () => {
+    Axios.post("https://trantam.herokuapp.com/api/form/submit", {
+      name: username,
+      email: email,
+      phone: phone,
+      notes: notes,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
   return (
     <div>
-      <h1>Họ và tên:</h1>
-      <input
-        id="name"
-        name="name"
-        value={val}
-        type="text"
-        placeholder="Họ và tên"
-      ></input>
-      <h1>Email:</h1>
-      <input
-        id="email"
-        name="email"
-        value={val}
-        type="email"
-        placeholder="Email"
-      ></input>
-      <h1>Số điện thoại:</h1>
-      <input
-        id="phone-number"
-        name="phonenumber"
-        value={val}
-        type="phone"
-        placeholder="Số điện thoại"
-      ></input>
-      <h1>Nội dung liên hệ:</h1>
-      <input
-        id="notes"
-        name="notes"
-        value={val}
-        type="text"
-        placeholder="Nội dung liên hệ"
-      ></input>
-      <button onClick={saveData}>Submit</button>
-      <button onClick={() => setVal(() => "")}>Clear</button>
+      <form>
+        <h2>Họ và tên:</h2>
+        <input
+          id="name"
+          name="username"
+          value={val}
+          type="text"
+          placeholder="Họ và tên"
+        ></input>
+        <h2>Email:</h2>
+        <input
+          id="email"
+          name="email"
+          value={val}
+          type="text"
+          placeholder="Email"
+        ></input>
+        <h2>Số điện thoại:</h2>
+        <input
+          id="phone-number"
+          name="phone"
+          value={val}
+          type="text"
+          placeholder="Số điện thoại"
+        ></input>
+        <h2>Nội dung liên hệ:</h2>
+        <input
+          id="notes"
+          name="notes"
+          value={val}
+          type="text"
+          placeholder="Nội dung liên hệ"
+        ></input>
+        <div className="btns">
+          <button onClick={saveData}>Submit</button>
+          <button onClick={() => setVal(() => "")}>Clear</button>
+        </div>
+      </form>
     </div>
   );
 };
